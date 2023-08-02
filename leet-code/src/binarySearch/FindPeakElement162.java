e302a82dd8edcc3298775a699b4221d3f42ea50f
vivek <vbhojawala@gmail.com>
2023-08-02 16:29:39 +0530

    FindPeakElement162

---
+++ leet-code/src/binarySearch/FindPeakElement162.java
@@ -0,0 +1,16 @@
+package binarySearch;
+
+public class FindPeakElement162 {
+    public int findPeakElement(int[] nums) {
+        int left =0, right = nums.length-1, mid = 0;
+
+        while (left < right){
+            mid = left + ( (right-left) / 2 );
+            if(nums[mid] > nums[mid+1])
+                right = mid;
+            else
+                left = mid +1;
+        }
+        return left;
+    }
+}
