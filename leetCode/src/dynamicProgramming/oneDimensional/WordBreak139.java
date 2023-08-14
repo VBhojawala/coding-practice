bca351df8a224fe1b7c97ea0e6bc663ffb1786fa
vivek <vbhojawala@gmail.com>
2023-08-14 09:36:01 +0530

    WordBreak139

---
+++ leetCode/src/dynamicProgramming/oneDimensional/WordBreak139.java
@@ -0,0 +1,31 @@
+package dynamicProgramming.oneDimensional;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak139 {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] isWordBreak = new boolean[n +1];
+        isWordBreak[n]=true;
+        for (int i=n-1; i>= 0; i--){
+           for (String word : wordDict){
+               if ( (i+word.length()) <= n && isWordBreak[i+word.length()]
+                       && s.substring(i, i+word.length()).equals(word)){
+                       isWordBreak[i] = true;
+                       break;
+                   }
+               }
+           }
+        return isWordBreak[0];
+    }
+
+    public static void main(String[] args) {
+        WordBreak139 prog = new WordBreak139();
+        System.out.println(prog.wordBreak("applepenapple", Arrays.asList("apple","pen")));
+        System.out.println(prog.wordBreak("catsandog", Arrays.asList("cats","dog","sand","and","cat")));
+    }
+}
