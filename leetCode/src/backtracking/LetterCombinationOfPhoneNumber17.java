caefc19cfd86321f5afcb64ab0fe4065fa4dba5d
vivek <vbhojawala@gmail.com>
2023-08-23 15:21:17 +0530

    LetterCombinationOfPhoneNumber17

--- leetCode/src/backtracking/LetterCombinationOfPhoneNumber17.java
+++ leetCode/src/backtracking/LetterCombinationOfPhoneNumber17.java
@@ -4,24 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCombinationOfPhoneNumber17 {
+
+    String digits;
+    int n;
+    String[] dial = new String[] {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    StringBuilder temp;
+
+    List<String> result;
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-
-        if(digits.isEmpty()) return result;
-
-        result.add("");
-        String[] chars = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-        for (char digit : digits.toCharArray()){
-            List<String> newResult = new ArrayList<>();
-            for (String x : result){
-                for (String y : chars[digit - '2'].split("")){
-                    newResult.add(x+y);
-                }
-            }
-            result = newResult;
-        }
+        this.digits = digits;
+        n = digits.length();
+        result = new ArrayList<>();
+        temp = new StringBuilder();
+        if(digits.isEmpty())
+            return result;
+        dfs(0);
         return result;
+
+    }
+
+    private void dfs(int i){
+        if (i == n){
+            result.add(temp.toString());
+            return;
+        }
+
+        if (i > n)
+            return;
+
+        for (char ch : dial[digits.charAt(i) - '2'].toCharArray()){
+            temp.append(ch);
+            dfs(i+1);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+
     }
 
     public static void main(String[] args) {
