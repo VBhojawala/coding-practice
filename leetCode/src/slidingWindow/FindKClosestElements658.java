78dd692d0ecb7feb3f279f34037ac0c8806bf35b
vivek <vbhojawala@gmail.com>
2023-08-29 21:39:07 +0530

    FindKClosestElements658

---
+++ leetCode/src/slidingWindow/FindKClosestElements658.java
@@ -0,0 +1,28 @@
+package slidingWindow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindKClosestElements658 {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<>();
+        int left =0, right= arr.length-1;
+
+        while ((right - left + 1) > k) {
+            if (Math.abs(arr[left] -x) > Math.abs(arr[right] -x))
+                left++;
+            else
+                right--;
+        }
+
+        for (int i=left; i<=right;i++)
+            result.add(arr[i]);
+
+        return result;
+    }
+    public static void main(String[] args) {
+        FindKClosestElements658 prog = new FindKClosestElements658();
+        System.out.println(prog.findClosestElements(new int[]{1,2,3,4,5}, 4,3));
+        System.out.println(prog.findClosestElements(new int[]{1,2,3,4,5}, 4,-1));
+    }
+}
