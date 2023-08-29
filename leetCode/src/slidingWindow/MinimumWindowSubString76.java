44fe3cf8ca05a66138a6b4e823dd6a3eabbf0785
vivek <vbhojawala@gmail.com>
2023-08-29 11:27:49 +0530

    MinimumWindowSubString76

--- leetCode/src/slidingWindow/MinimumWindowSubString76.java
+++ leetCode/src/slidingWindow/MinimumWindowSubString76.java
@@ -6,67 +6,40 @@ import java.util.Map;
 public class MinimumWindowSubString76 {
 
     public static String minWindow(String s, String t) {
+        int[] target = new int[128];
+        int[] widow = new int[128];
+        int m = s.length(), n= t.length();
+        for (int i=0;i<n;i++)
+            target[t.charAt(i)]++;
 
-        if (s.isEmpty())
-            return s;
+        int count=0, left=0, max = Integer.MAX_VALUE, startIndex=-1;
 
-        Map<Character, Integer> curWindow = new HashMap<>();
-        Map<Character, Integer> targetSet = new HashMap<>();
+        for (int right=0; right< m;right++){
 
-        for(int i =0; i< t.length(); i++){
-            if(targetSet.containsKey(t.charAt(i))){
-                targetSet.put(t.charAt(i), targetSet.get(t.charAt(i)) + 1 );
+            widow[s.charAt(right)]++;
+
+            if (target[s.charAt(right)] >= widow[s.charAt(right)]){
+                count++;
             }
-            else {
-                targetSet.put(t.charAt(i), 1);
+
+            while (count == n){
+                if ((right - left +1) < max){
+                    max = right-left + 1;
+                    startIndex = left;
+                }
+                if (target[s.charAt(left)] >= widow[s.charAt(left)]){
+                    count--;
+                }
+                widow[s.charAt(left++)]--;
             }
         }
-
-        int l =0;
-        int size = t.length();
-        String result = "";
-        int minLength =  Integer.MAX_VALUE;
-        for(int r=0; r < s.length(); r++){
-
-            if(targetSet.containsKey(s.charAt(r))){
-                if(curWindow.containsKey(s.charAt(r))){
-                    curWindow.put(s.charAt(r), curWindow.get(s.charAt(r)) + 1 );
-                }
-                else {
-                    curWindow.put(s.charAt(r), 1);
-                }
-            }
-
-            while (curWindow.size() == targetSet.size() && compareMaps(curWindow, targetSet)){
-                if(curWindow.containsKey(s.charAt(l))){
-                    if (curWindow.get(s.charAt(l)) == 1)
-                        curWindow.remove(s.charAt(l));
-                    else
-                        curWindow.put(s.charAt(l), curWindow.get(s.charAt(l)) - 1 );
-                }
-                if((r - l +1) < minLength){
-                    minLength = r - l +1;
-                    result = s.substring(l, r+1);
-                }
-                l++;
-            }
-
-        }
-        return  result;
+        return startIndex == -1 ? "" : s.substring(startIndex, startIndex+max);
     }
 
-    private static boolean compareMaps(Map<Character, Integer> s, Map<Character, Integer> t){
-        for (Character c: t.keySet()){
-            if(s.get(c) < t.get(c))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+
     public static void main(String[] args) {
 
-        String s = "AA", t = "AA";
+        String s = "ADOBECODEBANC", t = "ABC";
         System.out.println("Substring : " + minWindow(s, t));
 
 
