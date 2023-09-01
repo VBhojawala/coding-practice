f257cf078a223a39f91bd48b761404830049746b
vivek <vbhojawala@gmail.com>
2023-09-01 11:59:40 +0530

    LongestCommonPrefix14

--- leetCode/src/arrays/LongestCommonPrefix14.java
+++ leetCode/src/arrays/LongestCommonPrefix14.java
@@ -2,28 +2,14 @@ package arrays;
 
 public class LongestCommonPrefix14 {
     public static String longestCommonPrefix(String[] strs) {
-        String result = "";
-        boolean match = true;
-        for(int i=0;i<strs[0].length(); i++){
-            for(int j=1; j <strs.length; j++){
-                if(strs[j].length() <= i){
-                    match = false;
-                    break;
+        for (int i=0; i<strs[0].length(); i++){
+            for (int j=1; j< strs.length; j++){
+                if (strs[j].length() <= i || strs[j].charAt(i) != strs[0].charAt(i)){
+                    return strs[0].substring(0,i);
                 }
-                if(strs[j].length() > i && strs[0].charAt(i) !=strs[j].charAt(i)){
-                    match = false;
-                    break;
-                }
-
-            }
-            if(match){
-                result += strs[0].charAt(i);
-            }
-            else{
-                break;
             }
         }
-        return result;
+        return strs[0];
     }
 
     public static void main(String[] args) {
