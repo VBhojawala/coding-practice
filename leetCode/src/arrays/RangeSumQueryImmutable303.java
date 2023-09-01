9b6588ffb91bbf87e7e3ca93915b5731c332dc34
vivek <vbhojawala@gmail.com>
2023-09-01 16:05:55 +0530

    RangeSumQueryImmutable303

---
+++ leetCode/src/arrays/RangeSumQueryImmutable303.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class RangeSumQueryImmutable303 {
+
+    int[] sum;
+    public RangeSumQueryImmutable303(int[] nums) {
+        sum = new int[nums.length+1];
+        for (int i=0; i<nums.length; i++){
+            sum[i+1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sum[right+1] - sum[left];
+    }
+}
