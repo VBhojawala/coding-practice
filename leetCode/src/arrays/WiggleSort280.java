e73e1321aa4f76dc2f52e77486c70afbbaa165bd
vivek <vbhojawala@gmail.com>
2023-09-04 07:59:49 +0530

    WiggleSort280

---
+++ leetCode/src/arrays/WiggleSort280.java
@@ -0,0 +1,34 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class WiggleSort280 {
+
+    int[] nums;
+    int temp;
+
+    public void wiggleSort(int[] nums) {
+        this.nums = nums;
+        for (int i=1;i<nums.length;i++){
+            if ((i%2 == 1 && nums[i] < nums[i-1]) || (i%2 == 0 && nums[i] > nums[i-1])){
+                swap(i,i-1);
+            }
+        }
+
+    }
+
+    private void swap(int i, int j){
+        temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        WiggleSort280 prog = new WiggleSort280();
+
+        int[] nums = new int[] {3, 5, 2, 1, 6, 4};
+        prog.wiggleSort(nums);
+
+        Arrays.stream(nums).forEach(System.out::println);
+    }
+}
