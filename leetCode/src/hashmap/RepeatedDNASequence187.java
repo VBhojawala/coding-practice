f6f80c1ae07fa3c131f4f93542d2fd63751495f3
vivek <vbhojawala@gmail.com>
2023-09-04 20:59:52 +0530

    RepeatedDNASequence187

--- leetCode/src/hashmap/RepeatedDNASequence187.java
+++ leetCode/src/hashmap/RepeatedDNASequence187.java
@@ -6,16 +6,30 @@ public class RepeatedDNASequence187 {
     public List<String> findRepeatedDnaSequences(String s) {
         List<String> result = new ArrayList<>();
         if (s.length() < 10) return result;
-        Map<String, Integer> seen = new HashMap<>();
-        String pattern;
-        for (int i=9; i <s.length();i++){
-            pattern = s.substring(i-9, i+1);
-            seen.merge(pattern, 1, Integer::sum);
-            if (seen.get(pattern) == 2)
-                result.add(pattern);
+        Map<Integer, Integer> seen = new HashMap<>();
+        int hash = 0, mask = (1 << 27 ) - 1;
+
+        for (int i=0;i<9;i++)
+            hash = (hash << 3) | getVal(s.charAt(i));
+
+        for (int i=9;i<s.length();i++){
+            hash = (hash << 3) | getVal(s.charAt(i));
+            if (seen.merge(hash, 1, Integer::sum) == 2)
+                result.add(s.substring(i-9, i+1));
+            hash &= mask;
         }
+
         return result;
     }
+
+    private int getVal(char c){
+        if (c == 'A') return 1;
+        if (c == 'C') return 2;
+        if (c == 'G') return 3;
+        if (c == 'T') return 4;
+        return 0;
+    }
+
 
     public static void main(String[] args) {
         RepeatedDNASequence187 prog = new RepeatedDNASequence187();
