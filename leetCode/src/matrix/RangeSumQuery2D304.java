3dae48544516a9ac0847c76d33d83e8cafae56b4
vivek <vbhojawala@gmail.com>
2023-09-05 10:01:19 +0530

    RangeSumQuery2D304

---
+++ leetCode/src/matrix/RangeSumQuery2D304.java
@@ -0,0 +1,29 @@
+package matrix;
+
+public class RangeSumQuery2D304 {
+
+    int[][] sum;
+    public RangeSumQuery2D304(int[][] matrix) {
+        int m= matrix.length, n = matrix[0].length;
+        sum = new int[m+1][n+1];
+
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                sum[i+1][j+1] = sum[i][j+1] + sum[i+1][j] - sum[i][j] + matrix[i][j];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2+1][col2+1] - sum[row2+1][col1] - sum[row1][col2+1]+ sum[row1][col1];
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}};
+        RangeSumQuery2D304 prog = new RangeSumQuery2D304(matrix);
+        System.out.println(prog.sumRegion(2, 1, 4, 3));
+        System.out.println(prog.sumRegion(1, 1, 2, 2));
+        System.out.println(prog.sumRegion(1, 2, 2, 4));
+
+    }
+}
