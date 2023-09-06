0b9f730ea992885578122dd21d4224294dbd3db1
vivek <vbhojawala@gmail.com>
2023-09-06 09:54:00 +0530

    LongestCommonSubsequence1143

---
+++ leetCode/src/dynamicProgramming/twoDimensional/LongestCommonSubsequence1143.java
@@ -0,0 +1,24 @@
+package dynamicProgramming.twoDimensional;
+
+public class LongestCommonSubsequence1143 {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m= text1.length(), n= text2.length();
+        int dp[][] = new int[m+1][n+1];
+
+        for (int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if (text1.charAt(i) == text2.charAt(j))
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                else
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        LongestCommonSubsequence1143 prog = new LongestCommonSubsequence1143();
+        System.out.println(prog.longestCommonSubsequence("abcde", "ace"));
+    }
+}
