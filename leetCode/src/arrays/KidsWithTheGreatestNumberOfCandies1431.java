097458255bc758de48ee8feee071cdcdc0e08fdf
vivek <vbhojawala@gmail.com>
2023-09-13 10:45:58 +0530

    KidsWithTheGreatestNumberOfCandies1431

---
+++ leetCode/src/arrays/KidsWithTheGreatestNumberOfCandies1431.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies1431 {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for (int candy :candies)
+            max = Math.max(max, candy);
+        List<Boolean> result = new ArrayList<>();
+        for (int candy : candies)
+            result.add(candy + extraCandies >= max);
+        return result;
+    }
+}
