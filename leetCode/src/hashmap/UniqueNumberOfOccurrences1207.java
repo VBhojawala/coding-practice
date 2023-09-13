b866e94698c330549dd1cd32b6fb844e4122baf7
vivek <vbhojawala@gmail.com>
2023-09-13 12:16:23 +0530

    UniqueNumberOfOccurrences1207

---
+++ leetCode/src/hashmap/UniqueNumberOfOccurrences1207.java
@@ -0,0 +1,15 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class UniqueNumberOfOccurrences1207 {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : arr)
+            countMap.merge(num, 1, Integer::sum);
+        return new HashSet<>(countMap.values()).size() == countMap.size();
+    }
+}
