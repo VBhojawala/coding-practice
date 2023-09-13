9ac8d4c5a2ea4c48a6b97a003977ae09b118ec5c
vivek <vbhojawala@gmail.com>
2023-09-13 10:58:07 +0530

    IncreasingTripletSubsequence334

---
+++ leetCode/src/greedy/IncreasingTripletSubsequence334.java
@@ -0,0 +1,16 @@
+package greedy;
+
+public class IncreasingTripletSubsequence334 {
+    public boolean increasingTriplet(int[] nums) {
+        int min = Integer.MAX_VALUE, mid= Integer.MAX_VALUE;
+        for (int num : nums){
+            if (num > mid)
+                return true;
+            if (num <= min)
+                min = num;
+            else
+                mid= num;
+        }
+        return false;
+    }
+}
