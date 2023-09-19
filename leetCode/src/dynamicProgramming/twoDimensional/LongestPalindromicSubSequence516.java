ddf82fbfed96646cbc6b3a49cc24eee63da96279
vivek <vbhojawala@gmail.com>
2023-09-19 12:42:55 +0530

    LongestPalindromicSubSequence516

--- leetCode/src/dynamicProgramming/twoDimensional/LongestPalindromicSubSequence516.java
+++ leetCode/src/dynamicProgramming/twoDimensional/LongestPalindromicSubSequence516.java
@@ -5,16 +5,15 @@ public class LongestPalindromicSubSequence516 {
         int n = s.length();
         int[][] dp = new int[n][n];
 
-        for (int i=0; i<n; i++)
+        for (int i=n-1; i>=0;i--){
             dp[i][i] = 1;
-
-        for (int j=1; j<n;j++){
-            for (int i= j-1; i>=0; i--){
+            for (int j=i+1; j<n; j++){
                 if (s.charAt(i) == s.charAt(j))
-                    dp[i][j] = dp[i+1][j-1] +2;
+                    dp[i][j] = dp[i+1][j-1] + 2;
                 else
                     dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
             }
+
         }
 
         return dp[0][n-1];
