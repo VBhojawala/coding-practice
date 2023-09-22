cc6f6543f4f07de99f1d2382cadf9734585ce110
vivek <vbhojawala@gmail.com>
2023-09-22 12:20:26 +0530

    MeetingRoomII253

--- leetCode/src/interval/MeetingRoomII253.java
+++ leetCode/src/interval/MeetingRoomII253.java
@@ -3,20 +3,14 @@ import java.util.*;
 
 public class MeetingRoomII253 {
 
-    public static class Interval {
-        int start, end;
-        Interval(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
-    public int minMeetingRooms(List<Interval> intervals) {
-        int[] startTimes = new int[intervals.size()];
-        int[] endTimes = new int[intervals.size()];
 
-        for (int i=0 ;i <intervals.size();i++){
-            startTimes[i] = intervals.get(i).start;
-            endTimes[i] = intervals.get(i).end;
+    public int minMeetingRooms(int[][] intervals) {
+        int[] startTimes = new int[intervals.length];
+        int[] endTimes = new int[intervals.length];
+
+        for (int i=0 ;i <intervals.length;i++){
+            startTimes[i] = intervals[i][0];
+            endTimes[i] = intervals[i][1];
         }
 
         Arrays.sort(startTimes);
@@ -25,7 +19,7 @@ public class MeetingRoomII253 {
         int count=0, max=0;
         int start=0, end=0;
 
-        while (start < intervals.size()){
+        while (start < intervals.length){
             if (startTimes[start] < endTimes[end]){
                 start++;count++;
                 max = Math.max(max, count);
@@ -38,16 +32,6 @@ public class MeetingRoomII253 {
 
     public static void main(String[] args) {
         MeetingRoomII253 prog = new MeetingRoomII253();
-
-        List<Interval> intervals = new ArrayList<>();
-        intervals.add(new Interval(0,30));
-        intervals.add(new Interval(5,10));
-        intervals.add(new Interval(15,20));
-        System.out.println(prog.minMeetingRooms(intervals));
-
-        intervals.clear();
-        intervals.add(new Interval(2,7));
-        System.out.println(prog.minMeetingRooms(intervals));
 
     }
 }
