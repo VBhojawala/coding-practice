453a0299e9ef83fe2127895ac364b2247d7eaad2
vivek <vbhojawala@gmail.com>
2023-10-12 11:24:55 +0530

    RunningSumOfPrefixArray1480

---
+++ leetCode/src/arrays/prefixSum/RunningSumOfPrefixArray1480.java
@@ -0,0 +1,10 @@
+package arrays.prefixSum;
+
+public class RunningSumOfPrefixArray1480 {
+    public int[] runningSum(int[] nums) {
+        for (int i=1; i<nums.length;i++){
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+}
