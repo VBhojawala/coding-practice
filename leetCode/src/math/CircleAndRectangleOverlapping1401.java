e730d148a24cfd08bef5a3547f2b36a31b919ec7
vivek <vbhojawala@gmail.com>
2023-10-15 10:10:48 +0530

    CircleAndRectangleOverlapping1401

---
+++ leetCode/src/math/CircleAndRectangleOverlapping1401.java
@@ -0,0 +1,15 @@
+package math;
+
+public class CircleAndRectangleOverlapping1401 {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int closestX = Math.max(x1, Math.min(xCenter, x2));
+        int closestY = Math.max(y1, Math.min(yCenter, y2));
+
+        int distanceX = xCenter - closestX;
+        int distanceY = yCenter - closestY;
+
+        int squareDistance = distanceX* distanceX + distanceY * distanceY;
+
+        return squareDistance <= radius * radius;
+    }
+}
