9389de762e808c312e77a9c142958ae5d95063b8
vivek <vbhojawala@gmail.com>
2023-10-20 11:53:50 +0530

    CheckCompletenessOfBinaryTree958

---
+++ leetCode/src/binaryTree/bfs/CheckCompletenessOfBinaryTree958.java
@@ -0,0 +1,24 @@
+package binaryTree.bfs;
+
+import binaryTree.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class CheckCompletenessOfBinaryTree958 {
+    public boolean isCompleteTree(TreeNode root) {
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (queue.peek() != null){
+            TreeNode node = queue.poll();
+            queue.offer(node.left);
+            queue.offer(node.right);
+        }
+
+        while (!queue.isEmpty() && queue.peek() == null)
+            queue.poll();
+
+        return queue.isEmpty();
+    }
+}
