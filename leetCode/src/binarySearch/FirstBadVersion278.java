798139a05b6350788b9ba5c0666aa75bb62222b0
vivek <vbhojawala@gmail.com>
2023-11-04 07:52:16 +0530

    FirstBadVersion278

---
+++ leetCode/src/binarySearch/FirstBadVersion278.java
@@ -0,0 +1,22 @@
+package binarySearch;
+
+class VersionControl{
+    boolean isBadVersion(int version){
+        return false;
+    }
+}
+
+public class FirstBadVersion278 extends VersionControl{
+
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right){
+            int mid = left  + (right - left) / 2;
+            if (isBadVersion(mid))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+}
