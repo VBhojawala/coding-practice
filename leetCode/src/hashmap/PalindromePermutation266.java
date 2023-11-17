efa04612ab12e2c4a546e032ff3282dccd96106a
vivek <vbhojawala@gmail.com>
2023-11-17 09:32:42 +0530

    PalindromePermutation266

---
+++ leetCode/src/hashmap/PalindromePermutation266.java
@@ -0,0 +1,21 @@
+package hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PalindromePermutation266 {
+    public boolean canPermutePalindrome(String s) {
+        int[] charCount = new int[26];
+        for (char ch : s.toCharArray()){
+            charCount[ch-'a']++;
+        }
+        int count =0;
+
+        for (int i=0; i< 26; i++){
+            count += charCount[i] % 2;
+        }
+
+        return count <= 1;
+
+    }
+}
