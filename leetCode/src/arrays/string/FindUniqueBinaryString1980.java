f6af07261417e477852bc0573ab8b89d58913c38
vivek <vbhojawala@gmail.com>
2023-11-30 08:33:44 +0530

    FindUniqueBinaryString1980

---
+++ leetCode/src/arrays/string/FindUniqueBinaryString1980.java
@@ -0,0 +1,12 @@
+package arrays.string;
+
+public class FindUniqueBinaryString1980 {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder result = new StringBuilder();
+        for (int i=0; i< nums.length; i++){
+            char ch = nums[i].charAt(i);
+            result.append(ch == '0' ? '1' : '0');
+        }
+        return result.toString();
+    }
+}
